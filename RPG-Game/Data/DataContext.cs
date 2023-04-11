373d97ec6b4bb88f67dbd0972a3c598e7ab4ed1a
Maycon Jardim <mayconsmill@gmail.com>
2023-04-11 15:27:04 -0300

    Criando Seed para Skills e fazendo migração e atualizão para adicionar os dados.

--- RPG-Game/Data/DataContext.cs
+++ RPG-Game/Data/DataContext.cs
@@ -12,6 +12,17 @@ namespace RPG_Game.Data
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Skill>().HasData(
+                new Skill { Id = 1, Name = "Fireball", Damage = 30},
+                new Skill { Id = 2, Name = "Frenzy", Damage = 20 },
+                new Skill { Id = 3, Name = "Blizzard", Damage = 19 },
+                new Skill { Id = 4, Name = "Air Bazook", Damage = 40 },
+                new Skill { Id = 5, Name = "Dragon Slayer", Damage = 52 }
+                );
+        }
+
         public DbSet<Character> Characters { get; set; }
 
         public DbSet<User> Users { get; set; }
--- RPG-Game/Migrations/20230411182057_addSkill.Designer.cs
+++ RPG-Game/Migrations/20230411182057_addSkill.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RPG_Game.Data;
 
@@ -11,9 +12,11 @@ using RPG_Game.Data;
 namespace RPG_Game.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230411182057_addSkill")]
+    partial class addSkill
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -92,38 +95,6 @@ namespace RPG_Game.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Skills");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = 1,
-                            Damage = 30,
-                            Name = "Fireball"
-                        },
-                        new
-                        {
-                            Id = 2,
-                            Damage = 20,
-                            Name = "Frenzy"
-                        },
-                        new
-                        {
-                            Id = 3,
-                            Damage = 19,
-                            Name = "Blizzard"
-                        },
-                        new
-                        {
-                            Id = 4,
-                            Damage = 40,
-                            Name = "Air Bazook"
-                        },
-                        new
-                        {
-                            Id = 5,
-                            Damage = 52,
-                            Name = "Dragon Slayer"
-                        });
                 });
 
             modelBuilder.Entity("RPG_Game.Models.User", b =>
