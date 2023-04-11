06da873a9237e7b0284ab49421892c793e8d315c
Maycon Jardim <mayconsmill@gmail.com>
2023-04-10 21:40:01 -0300

    Criando AuthRepository

---
+++ RPG-Game/Data/IAuthRepository.cs
@@ -0,0 +1,15 @@
+﻿using RPG_Game.Models;
+
+namespace RPG_Game.Data
+{ 
+    public interface IAuthRepository
+    {
+
+        Task<ServiceResponse<int>> Register(User user, string password);
+
+        Task<ServiceResponse<string>> Login(User user, string password);
+
+        Task<bool> UserExists(String username);
+
+    }
+}
--- RPG-Game/Models/User.cs
+++ RPG-Game/Models/User.cs
@@ -6,7 +6,7 @@
         public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; }
-        public byte[] passwordSalt { get; set; }
+        public byte[] PasswordSalt { get; set; }
         public List<Character>? Characters { get; set; }
     }
 }
